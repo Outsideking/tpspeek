170eed18104c2432db34bbb4879e2a3b0444e6b3
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 23:00:10 +0700

    Update websocket.py

--- app/websocket.py
+++ app/websocket.py
@@ -1,1 +1,10 @@
 # Realtime translation
+from fastapi import WebSocket
+
+class RealtimeTranslator:
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        while True:
+            data = await websocket.receive_text()
+            # TODO: แปลข้อความทันที
+            await websocket.send_text(f"[Realtime Translation]: {data}")
