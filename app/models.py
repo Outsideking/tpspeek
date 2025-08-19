ae13ce2da0fda15277421dae47a391ee3772b96d
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 23:23:11 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,12 +1,11 @@
-# Database models (API Keys)
 from sqlalchemy import Column, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./tpspeek.db"
+DATABASE_URL = "sqlite:///./tpspeek_keys.db"
 
 Base = declarative_base()
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class APIKey(Base):
