cc1dfa4be32e78da090df12fdb71b966372534f0
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 23:59:41 +0700

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,1 +1,11 @@
+from fastapi import APIRouter
+from .models import SessionLocal, User
 
+router = APIRouter()
+
+@router.get("/admin/users")
+def get_users():
+    db = SessionLocal()
+    users = db.query(User).all()
+    db.close()
+    return users
