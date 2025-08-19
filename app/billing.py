286fcb37ad422917f5b49bbb1e44871ddd870d6d
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 23:57:56 +0700

    Update billing.py

--- app/billing.py
+++ app/billing.py
@@ -1,1 +1,6 @@
-
+def calculate_price(version:int, num_languages:int):
+    base_price = 100
+    lang_factor = 0.2 * num_languages
+    price_per_month = base_price + (base_price * lang_factor * (version-1))
+    price_per_year = price_per_month * 12
+    return price_per_month, price_per_year
