6cae2dd579e908000466c6aec9bd7c8297938aa4
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-20 01:06:01 +0700

    Update auth.py

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,1 +1,12 @@
+from fastapi import Request, HTTPException
+from .users import get_user_by_key
 
+async def verify_api_key(request: Request):
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or "Bearer " not in auth_header:
+        raise HTTPException(status_code=401, detail="Missing API Key")
+    token = auth_header.split(" ")[1]
+    user = get_user_by_key(token)
+    if not user:
+        raise HTTPException(status_code=403, detail="Invalid API Key")
+    return user
