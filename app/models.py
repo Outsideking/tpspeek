8258e4295593f5a04c7d64322d64d4d98808351f
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 23:55:25 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,13 +1,25 @@
-from sqlalchemy import Column, String, create_engine
+from sqlalchemy import Column, String, Integer, Float, DateTime, Boolean, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from datetime import datetime, timedelta
 
-DATABASE_URL = "sqlite:///./tpspeek_keys.db"
+DATABASE_URL = "sqlite:///./tpspeek_users.db"
 
 Base = declarative_base()
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-class APIKey(Base):
-    __tablename__ = "api_keys"
-    key = Column(String, primary_key=True, index=True)
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True, index=True)
+    api_key = Column(String, unique=True)
+    version = Column(Integer, default=1)
+    num_languages = Column(Integer, default=1)
+    monthly_price = Column(Float, default=100.0)
+    yearly_price = Column(Float, default=0.0)
+    plan_start = Column(DateTime, default=datetime.utcnow)
+    plan_end = Column(DateTime, default=datetime.utcnow() + timedelta(days=30))
+    active = Column(Boolean, default=True)
+    is_scanzaclip = Column(Boolean, default=False)
