bac90114cc49320183d4e1f0c81475e70bd1c679
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-22 04:29:15 +0700

    Update variables.tf

--- tpspeek-iac/variables.tf
+++ tpspeek-iac/variables.tf
@@ -1,1 +1,7 @@
-
+variable "region"       { type = string  default = "ap-southeast-2" }
+variable "account_id"   { type = string }
+variable "image_tag"    { type = string  default = "latest" } # tag ที่คุณ push ขึ้น ECR
+variable "task_cpu"     { type = number  default = 512 }
+variable "task_memory"  { type = number  default = 1024 }
+variable "desired_count"{ type = number  default = 1 }
+# variable "acm_certificate_arn" { type = string default = "" } # ใช้เมื่อเปิด HTTPS listener
