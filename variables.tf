88db6c5b71c8f0dcd55444dcc59068ba1d1a4c9f
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-22 04:13:01 +0700

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "region"         { type = string }
+variable "account_id"     { type = string }
+variable "log_group_name" { type = string }
+variable "old_kms_key_arn"{ type = string, default = "" }
