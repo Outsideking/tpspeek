c23e20f78f64947659f88ad7f00e43fe089c4ff6
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-22 04:29:37 +0700

    Update outputs.tf

--- tpspeek-iac/outputs.tf
+++ tpspeek-iac/outputs.tf
@@ -1,1 +1,4 @@
-
+output "alb_http_url"            { value = "http://${aws_lb.app.dns_name}" }
+output "api_gateway_invoke_url"  { value = aws_apigatewayv2_api.http.api_endpoint }
+output "ecr_repo"                { value = aws_ecr_repository.tpspeek.repository_url }
+output "log_group"               { value = aws_cloudwatch_log_group.app.name }
