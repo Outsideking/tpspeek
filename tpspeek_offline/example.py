54ca98e6a21ee089bf441c3919b2d8d4e11cbd0e
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-22 06:12:07 +0700

    Update example.py

--- tpspeek_offline/example.py
+++ tpspeek_offline/example.py
@@ -1,1 +1,9 @@
+from tpspeek.tpspeek import TPspeekOffline
 
+# เรียกใช้งาน
+tp = TPspeekOffline()
+
+# แปลงไฟล์เสียง input.wav -> แปล -> output.wav
+tp.speech_to_speech("input.wav", "output.wav")
+
+print("✅ เสร็จแล้ว! ดูไฟล์ output.wav")
