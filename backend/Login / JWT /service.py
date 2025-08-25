f8bb00f21dba772886f8dfd52f033902d16180d0
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-25 12:08:25 +0700

    Update service.py

--- backend/Login / JWT /service.py
+++ backend/Login / JWT /service.py
@@ -1,1 +1,10 @@
+from datetime import datetime, timedelta
+import jwt
+from app.backend.login.config import SECRET_KEY, ALGORITHM
 
+def create_access_token(data: dict, expires_delta: int = 60):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=expires_delta)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
